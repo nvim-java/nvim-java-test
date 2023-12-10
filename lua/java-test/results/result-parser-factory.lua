97a5a3d895c7672b83717be2d97797cd2d898f0c
Srinesh Nisala <srineshnisala@gmail.com>
2023-12-10 18:03:36 +0000

    feat: add lint & release-please workflows (#9)

--- lua/java-test/results/result-parser-factory.lua
+++ lua/java-test/results/result-parser-factory.lua
@@ -7,7 +7,7 @@ local TestParserFactory = class()
 ---Returns a test parser of given type
 ---@param args any
 ---@return java_test.TestParser
-function TestParserFactory.get_parser(args)
+function TestParserFactory.get_parser(_args)
 	return TestParser()
 end
 
